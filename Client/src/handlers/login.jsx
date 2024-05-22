d5d27781a28a78fbcebedb9696827c0070cbc6bd
jram828 <128335805+jram828@users.noreply.github.com>
2024-05-21 23:07:38 -0500

    incluir handlers

---
+++ Client/src/handlers/login.jsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
+import { setAuth } from "../redux/actions";
+import { useDispatch } from "react-redux";
+
+
+
+
+export async function Loginfo() {
+  const navigate = useNavigate();
+  try {
+    navigate("/home");
+  } catch (error) {
+    window.alert("Usuario o contraseña incorrectos");
+  }
+}
+
+export async function Loginf(userData) {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { email } = userData;
+  console.log("Datos login:", { email });
+  // const URL = "/clientes";
+  try {
+    const { data } = await axios(`/clientes/email?email=${email}`);
+    console.log("Login 2:", data);
+    const { access } = data;
+
+    if (email === data.correo) {
+        dispatch(setAuth(access));
+        navigate("/home");
+      } else {
+        window.alert("Usuario o contraseña incorrectos");
+      }
+  } catch (error) {
+    window.alert("Usuario o contraseña incorrectos");
+  }
+}
+//Acceder al modulo de crear usuario
+export const ClickHandlerCrear = (e) => {
+   const navigate = useNavigate();
+  e.preventDefault();
+  navigate("/crearusuario");
+};
+
+//Acceder al modulo de recordar contraseñas
+export const ClickHandlerRecordatorio = (e) => {
+   const navigate = useNavigate();
+  e.preventDefault();
+  navigate("/recordatoriocontrasena");
+};
